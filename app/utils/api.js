cb62bb7d02c154fe752305b4d04b619bd0661232
matssh <mats.hagen2@gmail.com>
2018-10-13 22:07:50 +0200

    api

--- app/utils/api.js
+++ app/utils/api.js
@@ -28,21 +28,62 @@ function getRepos(username) {
 
 }
 
-function getRepos(repos) {
+function getStarCount(repos) {
 
     return repos.data.reduce((count, repo) => {
         return count + repo.stargazers_count;
     }, 0);
+}
+
+function calculateScore(profile, repos) {
+
+    var followers = profile.followers;
+    var totalStars = getStarCount(repos);
+
+    return (followers * 3) + totalStars;
 
 }
 
+function handleError(error) {
+    console.warn(error);
+    return null;
+}
+
+function getUserData(player) {
+    return axios.all([
+        getProfile(player),
+        getRepos(player)
+    ]).then((data) => {
+        var profile = data[0];
+        var repos = data[1];
+
+        return {
+            profile: profiel,
+            score: calculateScore(profile, repos)
+        }
+
+    })
+}
+
+function sortPlayer(players) {
+
+    return players.sort((a, b) => {
+        return b.score - a.score;
+    })
+
+}
+
+
+
+
 module.exports = {
 
+
     battle: (players) => {
-
+        return axios.all(players.map(getUserData))
+            .then(sortPlayers)
+            .catch(handleError)
     },
-
-
 
     fetchPopularRepos: (language) => {
         var encodedURI = window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:' + language + '&sort=stars&order=desc&type=Repositories');
