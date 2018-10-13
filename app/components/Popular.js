d48c41479bee0401f28a76066bf396cd429052b5
matssh <mats.hagen2@gmail.com>
2018-10-13 11:28:42 +0200

    finished popular components

---
+++ app/components/Popular.js
@@ -0,0 +1,116 @@
+var React = require('react');
+var PropTypes = require('prop-types');
+var api = require('../utils/api');
+
+
+
+function SelectLanguage(props) {
+    var languages = ["All", "JavaScript", "Ruby", "Java", "CSS", "Python"];
+    return (
+        <div>
+            <div ><ul className="languages">
+                {languages.map((lang) => {
+                    return (
+                        <li
+                            style={lang === props.selectedLanguage ? { color: '#d0021b' } : null}
+                            onClick={props.onSelect.bind(null, lang)}
+                            key={lang}>
+                            {lang}
+                        </li>
+                    )
+
+                })}</ul></div>
+        </div>
+    )
+}
+
+function RepoGrid(props) {
+
+    return (
+        <ul className="popular-list">
+            {props.repos.map((repo, index) => {
+                return (
+                    <li key={repo.name} className="popular-item">
+                        <div className="popular-rank">
+                            #{index + 1}
+                        </div>
+                        <ul className="space-list-items">
+                            <li>
+                                <img src={repo.owner.avatar_url} alt={'Avatar for ' + repo.owner.login} className="avatar" />
+                            </li>
+                            <li><a href={repo.html_url}> {repo.name}</a>
+                            </li>
+                            <li>@{repo.owner.login}</li>
+                            <li>{repo.stargazers_count} stars</li>
+                        </ul>
+                    </li>
+                )
+            })}
+
+        </ul>
+    )
+
+}
+
+RepoGrid.propTypes = {
+    repos: PropTypes.array.isRequired,
+}
+
+
+SelectLanguage.propTypes = {
+    selectedLanguage: PropTypes.string.isRequired,
+    onSelect: PropTypes.func.isRequired
+}
+
+class Popular extends React.Component {
+
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedLanguage: 'All'
+        };
+
+        this.updateLanguage = this.updateLanguage.bind(this);
+    }
+
+    componentDidMount() {
+        this.updateLanguage(this.state.selectedLanguage);
+    }
+
+
+    updateLanguage(lang) {
+        this.setState(() => {
+            return {
+                selectedLanguage: lang,
+                repos: null
+            }
+        });
+
+        api.fetchPopularRepos(lang)
+            .then((repos) => {
+                this.setState(() => {
+                    return {
+                        repos: repos
+                    }
+
+                })
+            });
+
+    }
+
+    render() {
+        return (
+            <div>
+                <SelectLanguage
+                    selectedLanguage={this.state.selectedLanguage}
+                    onSelect={this.updateLanguage}
+                />
+                {!this.state.repos ? <p>LOADING</p> : <RepoGrid repos={this.state.repos} />}
+
+            </div>
+        )
+    }
+}
+
+module.exports = Popular;
--- app/index.js
+++ app/index.js
@@ -1,18 +1,9 @@
 var React = require("react");
 var ReactDOM = require("react-dom");
 require('./index.css');
+var App = require('./components/App');
 
-// COMPONENT:
 
-// state
-// lifecycle-events
-// hook into different lifecycle moments
-// UI
 
-class App extends React.Component {
-    render() {
-        return <div> Hello World! </div>;
-    }
-}
 
 ReactDOM.render(<App />, document.getElementById("app"));
