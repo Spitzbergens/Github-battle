956d4fc62926023632de477b6a730e0a81fe0e66
matssh <mats.hagen2@gmail.com>
2018-10-16 09:40:52 +0200

    ES6 refactor

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,86 +1,76 @@
 var React = require('react');
 var PropTypes = require('prop-types');
 var api = require('../utils/api');
-
-
+var Loading = require('./Loading');
 
 function SelectLanguage(props) {
-    var languages = ["All", "JavaScript", "Ruby", "Java", "CSS", "Python"];
+    var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
     return (
-        <div>
-            <div ><ul className="languages">
-                {languages.map((lang) => {
-                    return (
-                        <li
-                            style={lang === props.selectedLanguage ? { color: '#d0021b' } : null}
-                            onClick={props.onSelect.bind(null, lang)}
-                            key={lang}>
-                            {lang}
-                        </li>
-                    )
-
-                })}</ul></div>
-        </div>
+        <ul className='languages'>
+            {languages.map(function (lang) {
+                return (
+                    <li
+                        style={lang === props.selectedLanguage ? { color: '#d0021b' } : null}
+                        onClick={props.onSelect.bind(null, lang)}
+                        key={lang}>
+                        {lang}
+                    </li>
+                )
+            })}
+        </ul>
     )
 }
 
 function RepoGrid(props) {
-
     return (
-        <ul className="popular-list">
-            {props.repos.map((repo, index) => {
+        <ul className='popular-list'>
+            {props.repos.map(function (repo, index) {
                 return (
-                    <li key={repo.name} className="popular-item">
-                        <div className="popular-rank">
-                            #{index + 1}
-                        </div>
-                        <ul className="space-list-items">
+                    <li key={repo.name} className='popular-item'>
+                        <div className='popular-rank'>#{index + 1}</div>
+                        <ul className='space-list-items'>
                             <li>
-                                <img src={repo.owner.avatar_url} alt={'Avatar for ' + repo.owner.login} className="avatar" />
+                                <img
+                                    className='avatar'
+                                    src={repo.owner.avatar_url}
+                                    alt={'Avatar for ' + repo.owner.login}
+                                />
                             </li>
-                            <li><a href={repo.html_url}> {repo.name}</a>
-                            </li>
+                            <li><a href={repo.html_url}>{repo.name}</a></li>
                             <li>@{repo.owner.login}</li>
                             <li>{repo.stargazers_count} stars</li>
                         </ul>
                     </li>
                 )
             })}
-
         </ul>
     )
-
 }
 
 RepoGrid.propTypes = {
     repos: PropTypes.array.isRequired,
 }
 
-
 SelectLanguage.propTypes = {
     selectedLanguage: PropTypes.string.isRequired,
-    onSelect: PropTypes.func.isRequired
-}
+    onSelect: PropTypes.func.isRequired,
+};
 
 class Popular extends React.Component {
-
-
     constructor(props) {
-        super(props);
+        super();
         this.state = {
-            selectedLanguage: 'All'
+            selectedLanguage: 'All',
+            repos: null,
         };
 
         this.updateLanguage = this.updateLanguage.bind(this);
     }
-
     componentDidMount() {
-        this.updateLanguage(this.state.selectedLanguage);
+        this.updateLanguage(this.state.selectedLanguage)
     }
-
-
     updateLanguage(lang) {
-        this.setState(() => {
+        this.setState(function () {
             return {
                 selectedLanguage: lang,
                 repos: null
@@ -88,26 +78,23 @@ class Popular extends React.Component {
         });
 
         api.fetchPopularRepos(lang)
-            .then((repos) => {
-                this.setState(() => {
+            .then(function (repos) {
+                this.setState(function () {
                     return {
                         repos: repos
                     }
-
-                })
-            });
-
+                });
+            }.bind(this));
     }
-
     render() {
         return (
             <div>
                 <SelectLanguage
                     selectedLanguage={this.state.selectedLanguage}
-                    onSelect={this.updateLanguage}
-                />
-                {!this.state.repos ? <p>LOADING</p> : <RepoGrid repos={this.state.repos} />}
-
+                    onSelect={this.updateLanguage} />
+                {!this.state.repos
+                    ? <Loading />
+                    : <RepoGrid repos={this.state.repos} />}
             </div>
         )
     }
--- app/utils/api.js
+++ app/utils/api.js
@@ -1,47 +1,26 @@
-var axios = require('axios');
 
-var id = "YOUR_CLIENT_ID";
-var sec = "YOUR_SECRET_ID";
-var params = "?client_id=" + id + "&client_secret=" + sec;
+const axios = require('axios');
 
-
-/** Hent data. Promise returnerer brukerdata. */
+const id = 'YOUR_CLIENT_ID'
+const sec = "YOUR_SECRET_ID";
+const params = `?client_id=${id}&client_secret=${sec}`;
 
 function getProfile(username) {
-
-    return axios.get('https://api.github.com/users/' + username + params)
-        .then((user) => {
-            /** Dataen formateres før den sendes til sluttbruker
-             * Istedefor å få et object "user", får man "user.data"
-             * når metoden faktisk blir kalt
-             */
-            return user.data;
-        })
-
+    return axios.get(`https://api.github.com/users/${username}${params}`)
+        .then((user) => user.data);
 }
 
-// Når du kaller funksjonen, returneres også et promise
-
-
 function getRepos(username) {
-    return axios.get('https://api.github.com/users/' + username + "/repos" + params + "&per_page=100")
-
+    return axios.get(`https://api.github.com/users/${username}/repos${params}&per_page=100`);
 }
 
 function getStarCount(repos) {
-
-    return repos.data.reduce((count, repo) => {
-        return count + repo.stargazers_count;
-    }, 0);
+    return repos.data.reduce((count, repo) => count + repo.stargazers_count, 0);
 }
 
-function calculateScore(profile, repos) {
+function calculateScore({ followers }, repos) {
 
-    var followers = profile.followers;
-    var totalStars = getStarCount(repos);
-
-    return (followers * 3) + totalStars;
-
+    return (followers * 3) + getStarCount(repos);
 }
 
 function handleError(error) {
@@ -50,45 +29,31 @@ function handleError(error) {
 }
 
 function getUserData(player) {
-    return axios.all([
+    return Promise.all([
         getProfile(player),
         getRepos(player)
-    ]).then((data) => {
-        var profile = data[0];
-        var repos = data[1];
+    ]).then(([profile, repos]) => ({
 
-        return {
-            profile: profiel,
-            score: calculateScore(profile, repos)
-        }
+        profile,
+        score: calculateScore(profile, repos)
 
-    })
+    }));
 }
 
-function sortPlayer(players) {
-
-    return players.sort((a, b) => {
-        return b.score - a.score;
-    })
-
+function sortPlayers(players) {
+    return players.sort((a, b) => b.score - a.score);
 }
-
-
-
 
 module.exports = {
-
-
     battle: (players) => {
-        return axios.all(players.map(getUserData))
+        return Promise.all(players.map(getUserData))
             .then(sortPlayers)
-            .catch(handleError)
+            .catch(handleError);
     },
-
     fetchPopularRepos: (language) => {
-        var encodedURI = window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:' + language + '&sort=stars&order=desc&type=Repositories');
-        return axios.get(encodedURI).then((result) => {
-            return result.data.items;
-        });
+        const encodedURI = window.encodeURI(`https://api.github.com/search/repositories?q=stars:>1+language:${language}&sort=stars&order=desc&type=Repositories`);
+
+        return axios.get(encodedURI)
+            .then((response) => response.data.items);
     }
-}
+};
